2eaa1ebdc6921a10118634733dae5edc04564c55
Horyun Lee <ghfusdl7201@gmail.com>
2024-07-09 20:19:47 +0900

    not saved video

--- test10_5_5.py
+++ test10_5_5.py
@@ -1,10 +1,10 @@
 import cv2
-import torch
 import os
+import torch
 import numpy as np
 from facenet_pytorch import MTCNN, InceptionResnetV1
 from ultralytics import YOLO
-from collections import Counter
+from multiprocessing import Process
 
 class FaceRecognizer:
     def __init__(self, device=None):
@@ -37,7 +37,7 @@ class FaceRecognizer:
     def compare_similarity(embedding1, embedding2):
         return np.dot(embedding1, embedding2) / (np.linalg.norm(embedding1) * np.linalg.norm(embedding2))
 
-    def recognize_faces(self, frame, frame_number, output_dir, video_name, gender_model):
+    def recognize_faces(self, frame, frame_number, output_dir, video_name):
         original_shape = frame.shape[:2]
         frame_resized = cv2.resize(frame, (640, 480))
         frame_rgb = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB)
@@ -70,13 +70,6 @@ class FaceRecognizer:
                 box[3] = int(box[3] * scale_y)
                 cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2)
 
-                # Get gender prediction
-                gender = predict_gender(face_image, gender_model)
-
-                # Draw gender text below the bounding box
-                gender_text = f"Gender: {gender}"
-                cv2.putText(frame, gender_text, (box[0], box[3] + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-
                 # Draw ID text above the bounding box
                 id_text = f"ID: {self.person_id}"
                 cv2.putText(frame, id_text, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
@@ -84,63 +77,42 @@ class FaceRecognizer:
         return frame
 
 
-def predict_gender(face_image, gender_model):
-    face_rgb = cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB)
-    results = gender_model.predict(source=[face_rgb], save=False)
-    genders = {0: "Male", 1: "Female"}
-    gender_id = results[0].boxes.data[0][5].item()
-    return genders.get(gender_id, "Unknown")
+def process_video(video_path, output_dir):
+    video_name = os.path.splitext(os.path.basename(video_path))[0]
+    recognizer = FaceRecognizer()
 
+    v_cap = cv2.VideoCapture(video_path)
+    frame_number = 0
 
-def process_videos(video_paths, output_dir, yolo_model_path, gender_model_path, target_fps=10):
-    for video_path in video_paths:
-        video_name = os.path.splitext(os.path.basename(video_path))[0]
-        frame_width, frame_height = None, None
-        video_writer = None
+    while True:
+        success, frame = v_cap.read()
+        if not success:
+            break
+        frame_number += 1
 
-        v_cap = cv2.VideoCapture(video_path)
-        frame_rate = int(v_cap.get(cv2.CAP_PROP_FPS))
-        frame_interval = frame_rate // target_fps
+        frame = recognizer.recognize_faces(frame, frame_number, output_dir, video_name)
 
-        recognizer = FaceRecognizer()
-        yolo_model = YOLO(yolo_model_path)
-        gender_model = YOLO(gender_model_path)
-        frame_number = 0
+        cv2.imshow('Frame', frame)
 
-        while True:
-            success, frame = v_cap.read()
-            if not success:
-                break
-            frame_number += 1
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-            if frame_number % frame_interval != 0:
-                continue
-
-            frame = recognizer.recognize_faces(frame, frame_number, output_dir, video_name, gender_model)
-
-            if frame_width is None or frame_height is None:
-                frame_height, frame_width = frame.shape[:2]
-                output_video_path = os.path.join(output_dir, f"{video_name}_output.mp4")
-                video_writer = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), target_fps, (frame_width, frame_height))
-
-            video_writer.write(frame)
-
-            cv2.imshow('Frame with Bounding Boxes', frame)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        v_cap.release()
-        if video_writer:
-            video_writer.release()
-        cv2.destroyAllWindows()
-
+    v_cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    video_paths = ["./test/testVideo1.mp4", "./test/testVideo2.mp4"]  # 리스트로 여러 비디오 경로 설정
+    video_paths = ["./test/testVideo.mp4", "./test/testVideo2.mp4"]
     output_directory = "./output/"
-    yolo_model_path = './models/yolov8x.pt'
-    gender_model_path = './models/gender_model.pt'
 
-    process_videos(video_paths, output_directory, yolo_model_path, gender_model_path)
+    # Process each video in parallel using separate processes
+    processes = []
+    for video_path in video_paths:
+        p = Process(target=process_video, args=(video_path, output_directory))
+        p.start()
+        processes.append(p)
+
+    # Wait for all processes to finish
+    for p in processes:
+        p.join()
+
